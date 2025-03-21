0b0523564f1ae13863d0941865cd4f25b10b1a72
Березкин Александр <3.commm@gmail.com>
2025-03-21 13:14:42 +0300

    Создан пустой проект

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/Header/Header';
+import './styles/app.css';
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <main></main>
+    </>
+  );
+}
+
+export default App;
